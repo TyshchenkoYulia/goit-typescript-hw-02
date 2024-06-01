990df75f93222d8f12c3ea41c102b4887903434f
TyshchenkoYulia <145122847+TyshchenkoYulia@users.noreply.github.com>
2024-06-01 12:23:41 +0300

    update searchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -11,7 +11,7 @@ export const SearchBar: React.FC<SearchBarProps> = ({ onSubmit }) => {
   const onSubmitBar = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const form = event.target as HTMLFormElement;
-    const data = form.elements.topic.value as HTMLFormElement;
+    const data = form.elements.namedItem("topic") as HTMLFormElement;
 
     if (data.trim() === "") {
       toast.error("Please, enter your request!");
